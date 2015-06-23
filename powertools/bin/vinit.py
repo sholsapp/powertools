b07c72237a96c99ab47618c1af2fb699d4feb916
Stephen Holsapple <sholsapp@gmail.com>
2015-06-22 21:45:33 -0700

    Repurpose -r command line flag
    Repurpose the -r flag to specify bootstrap requirements that should be
    installed after the virtualenv is created.
    
    Renamed existing -r flag to -n (--new) to recreate the environment.

--- powertools/bin/vinit.py
+++ powertools/bin/vinit.py
@@ -9,7 +9,7 @@ Use *vinit* in the following way.
 
   $ git clone https://github.com/sholsapp/powertools
   $ cd powertools
-  $ vinit
+  $ vinit -r tox -r requests
   $ source activate
   $ python setup.py develop
 
@@ -42,21 +42,22 @@ def main():
                                    formatter_class=argparse.RawDescriptionHelpFormatter)
   parser.add_argument('-d', '--dest-directory', default=default_virtualenv,
                       help='Where to create the virtualenv instead of %(default)s.')
-  parser.add_argument('-r', '--recreate', action='store_true',
-                      help='Recreate the virtualenv if it already exists.')
+  parser.add_argument('-n', '--new', action='store_true',
+                      help='Create a new virtualenv if it already exists.')
+  parser.add_argument('-r', '--requirement', action='append', default=[],
+                      help='Pip-style requirements to bootstrap the virtualenv with.')
   args = parser.parse_args()
 
-  if not os.path.exists(args.dest_directory) or args.recreate:
+  if not os.path.exists(args.dest_directory) or args.new:
     if os.path.exists(args.dest_directory):
       shutil.rmtree(args.dest_directory)
-    subprocess.check_call([
-      'virtualenv',
-      args.dest_directory,
-    ])
+    subprocess.check_call(['virtualenv', args.dest_directory])
+    subprocess.check_call(['pip', 'install', 'pip', '--upgrade'])
+
+  for requirement in args.requirement:
+    subprocess.check_call(['pip', 'install', requirement])
 
   if not os.path.exists('activate'):
-    subprocess.check_call([
-      'ln', '-s', os.path.join(args.dest_directory, 'bin', 'activate'), 'activate',
-    ])
+    subprocess.check_call(['ln', '-s', os.path.join(args.dest_directory, 'bin', 'activate'), 'activate'])
 
   print u"Congratulations! \U0001F37A"
